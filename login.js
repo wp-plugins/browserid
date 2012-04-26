0b6ac6898e63d56d8894ab4084f19a7cda1ec062
M66B <M66B@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-26 19:41:49 +0000

    0.28 dev post assertion readme
    git-svn-id: https://plugins.svn.wordpress.org/browserid/trunk@536953 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- login.js
+++ login.js
@@ -1,26 +1,10 @@
-//function browserid_login() {
-//	navigator.id.getVerifiedEmail(function(assertion) {
-//		if (assertion) {
-//			rememberme = document.getElementById('rememberme');
-//			if (rememberme != null)
-//				rememberme = rememberme.checked;
-//			var url = browserid_siteurl + '?browserid_assertion=' + assertion + '&rememberme=' + rememberme;
-//			if (browserid_redirect != null)
-//				url += '&redirect_to=' + browserid_redirect;
-//			window.location.href = url;
-//		}
-//		else
-//			alert(browserid_failed);
-//	});
-//	return false;
-//}
-
 function browserid_login() {
 	navigator.id.getVerifiedEmail(function(assertion) {
 		if (assertion) {
 			rememberme = document.getElementById('rememberme');
 			if (rememberme != null)
 				rememberme = rememberme.checked;
+
 			var form = document.createElement('form');
 			form.method = 'POST';
 			form.action = browserid_siteurl;
@@ -32,21 +16,21 @@ function browserid_login() {
 			assertion_field.value = assertion;
 			form.appendChild(assertion_field);
 
-			var remem_field = document.createElement('input');
-			remem_field.type = 'hidden';
-			remem_field.name = 'rememberme';
-			remem_field.value = rememberme;
-			form.appendChild(remem_field);
+			var rememberme_field = document.createElement('input');
+			rememberme_field.type = 'hidden';
+			rememberme_field.name = 'rememberme';
+			rememberme_field.value = rememberme;
+			form.appendChild(rememberme_field);
 
 			if (browserid_redirect != null) {
-				var remem_field = document.createElement('input');
+				var redir_field = document.createElement('input');
 				redir_field.type = 'hidden';
 				redir_field.name = 'redirect_to';
 				redir_field.value =  browserid_redirect;
 				form.appendChild(redir_field);
 			}
-			var my_form = document.body.appendChild(form);
-			my_form.submit();
+			var assertion_form = document.body.appendChild(form);
+			assertion_form.submit();
 		}
 		else
 			alert(browserid_failed);
