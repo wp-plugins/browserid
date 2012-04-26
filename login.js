7606545c430865d95eee6e350dd9117976a51591
M66B <M66B@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-26 21:06:59 +0000

    0.28 dev improved login.js
    git-svn-id: https://plugins.svn.wordpress.org/browserid/trunk@536980 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- login.js
+++ login.js
@@ -1,36 +1,31 @@
 function browserid_login() {
 	navigator.id.getVerifiedEmail(function(assertion) {
 		if (assertion) {
-			rememberme = document.getElementById('rememberme');
+			var rememberme = document.getElementById('rememberme');
 			if (rememberme != null)
 				rememberme = rememberme.checked;
 
 			var form = document.createElement('form');
+			form.setAttribute('style', 'display: none;');
 			form.method = 'POST';
 			form.action = browserid_siteurl;
-			form.id = 'assertion_send_form';
 
-			var assertion_field = document.createElement('input');
-			assertion_field.type = 'hidden';
-			assertion_field.name = 'browserid_assertion';
-			assertion_field.value = assertion;
-			form.appendChild(assertion_field);
+			var fields =
+				[{name: 'browserid_assertion', value: assertion},
+				{name: 'rememberme', value: rememberme}];
 
-			var rememberme_field = document.createElement('input');
-			rememberme_field.type = 'hidden';
-			rememberme_field.name = 'rememberme';
-			rememberme_field.value = rememberme;
-			form.appendChild(rememberme_field);
+			if (browserid_redirect != null)
+				fields.push({name: 'redirect_to', value: browserid_redirect});
 
-			if (browserid_redirect != null) {
-				var redir_field = document.createElement('input');
-				redir_field.type = 'hidden';
-				redir_field.name = 'redirect_to';
-				redir_field.value =  browserid_redirect;
-				form.appendChild(redir_field);
+			for (var i = 0; i < fields.length; i++) {
+				var field = document.createElement('input');
+				field.type = 'hidden';
+				field.name = fields[i].name;
+				field.value = fields[i].value;
+				form.appendChild(field);
 			}
-			var assertion_form = document.body.appendChild(form);
-			assertion_form.submit();
+
+			document.body.appendChild(form).submit();
 		}
 		else
 			alert(browserid_failed);
